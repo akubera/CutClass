05ab9f373a069405917a994e8a62442b3889ed12
Andrew Kubera <andrewkubera@gmail.com>
2013-12-20 19:50:15 -0500

    Created a typedef for testing functions which can be run.

--- Makefile
+++ Makefile
@@ -8,10 +8,10 @@ CXX = g++
 all: build test
 
 
-test: build/main.o
-	${CXX} ${CXX_FLAGS} $< -o $@
+test: build/main.o build/Cut.o
+	${CXX} ${CXX_FLAGS} $^ -o $@
 
-build/%.o: src/%.cpp
+build/%.o: src/%.cpp src/%.hpp
 	${CXX} ${CXX_FLAGS} -c $< -o $@
 
 build:
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 
 #include <iostream>
 
+#include "Cut.hpp"
+
+bool atest(float x) {
+  return x < 2.0;
+}
+
+
 int main()
 {
+  test_func tes = {0};
+  tes.f_func = atest;
+  
+  Cut c0("c0", tes);
+  
+  std::cout << c0.Run(1.0f) << ' ' << c0.Run(9.0f) << std::endl;
+  
   std::cout << "It works!" << std::endl;
   return 0;
 }
